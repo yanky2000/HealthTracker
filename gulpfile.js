e48977080fd9489a3812595d83f576e80904506e
VY <yanky2000@yahoo.com>
2016-05-01 20:39:13 +0300

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var debug = require('gulp-debug');
 var gulpIf = require('gulp-if');
-var newer = require('gulp-newer');
+var newer = require('gulp-newer'); // process only new&modified files
 var del = require('del');
 var prefixer = require('autoprefixer');
 var autoprefixer = require('gulp-autoprefixer');
-var cached = require('cached'); // Returns only files with differed content(&names?) to it's cached ones.
+var cached = require('cached'); // Returns only files with differed content(&names?) to it's cached ones. Usually use since:
 var remember = require('remember'); /* Returns cached files content not presented in input argument */
 var path = require('path');
+var browserSync = require('browser-sync').create();
 
 var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development'; // to insert sourcemap only to development version. exclude in production
 
@@ -38,51 +39,22 @@ gulp.task('styles', function () {
         // .pipe(concat('all.css'))
         // .pipe(debug({title: 'sass'}))
         .pipe(gulpIf(isDevelopment, sourcemaps.write())) // If sourcemap.write{'.'} sourcefile will be created in separate file
-        .pipe(gulp.dest('dist/styles'));
+        .pipe(gulp.dest('dist/assets/css'));
     // .pipe(debug({title: 'dest'}))
 });
-
-gulp.task('styles2', function () {
-    return gulp.src('dev/assets/sass/**/*.sass')
-        .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-        .pipe(sass({ indentedSyntax: true }).on('error', sass.logError))
-        // .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-        .pipe(gulp.dest('dist/styles'));
-});
-
-
-gulp.task('styles3', function () {
-    return gulp.src('dev/assets/sass/**/*.*')
-        // .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-        .pipe(autoprefixer())
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('dist/styles'));
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* Apply to separate css files */
 // Should process only new&modified files, delete removed files, concat many files in one.
 gulp.task('stylesConcat', function () {
     return gulp.src('dev/**/*.*')
         .pipe(cached('stylesConcat')) // instead of adding {since: gulp.lastRun('stylesConcat')} to gulp.src, to prevent ctrl-z bug of deleted files.
-        .pipe(autoprefixer())
         .pipe(remember('stylesConcat'))
         .pipe(concat('all.css'))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('dist/assets'))
 })
+
+
 
 
 /* ========= WATCHERS ==========*/
@@ -100,12 +72,11 @@ gulp.task('styles:watchDeleted', function () {
  });
 
 
-
-
 /* Clean up dist directory */
 gulp.task('clean', function (tylesii) {
     return del('dist');
 });
+
 
 /* Copy all assets to dist */
 gulp.task('assets', function () {
@@ -113,11 +84,12 @@ gulp.task('assets', function () {
 
     // return with gulp.src('assets/**/*.{js,css}') - {js,css} all files with these extensions 
     // operates only for files, changed since last run of 'html' task
-    return gulp.src('dev/assets/**/*.*', { since: gulp.lastRun('assets') }) // starts filtering after assets run for 2nd time
-        .pipe(newer('dist/assets')) //process only new&changed files compared to already existing ones
+    return gulp.src('dev/**/*.*', { since: gulp.lastRun('assets') }) // starts filtering after assets run for 2nd time
+        .pipe(newer('dist')) //process only new&changed files compared to already existing ones, but doesn't respond to files deletion.
         .pipe(debug({ title: 'assets' }))
-        .pipe(gulp.dest('dist/assets'));
+        .pipe(gulp.dest('dist'));
 });
+
 
 /* Makes first build of dist */
 gulp.task('build', gulp.series(
@@ -125,14 +97,27 @@ gulp.task('build', gulp.series(
     gulp.parallel('assets', 'styles')
 ));
 
+
 /* Watch for changes in styles and assets */
 gulp.task('watch', function () {
-    gulp.watch('dev/assets/sass/**/*.sass', gulp.series('styles'));//gulp.series('styles', ...)
-    gulp.watch('dev/assets/**/*.*', gulp.series('assets'));
+    gulp.watch('dev/assets/sass/**/*.sass', gulp.series('styles'));
+    gulp.watch('dev/**/*.*', gulp.series('assets'));
+});
+
+/* ========== Browser Sync ==========*/
+gulp.task('serve', function () {
+    browserSync.init({
+        server: 'dist'
+    });
+
+    browserSync.watch('dist/**/*.*').on('change', browserSync.reload)
 });
 
 /* Init dist version */
-gulp.task('dev', gulp.series('build', 'watch'));
+gulp.task('dev',
+    gulp.series('build', gulp.parallel('watch', 'serve'))
+);
+
 
 
 
